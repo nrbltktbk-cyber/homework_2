008456c84d0ab6088a989710f225f6ba61bbf3cb
NurbolToktobekov <nrbltktbk-cyber@example.com>
2026-01-10 20:16:23 +0600

    hw2

---
+++ homework_3.py
@@ -0,0 +1,74 @@
+# homework_3.py
+
+class Person:
+    def __init__(self, name, birth_date, occupation, higher_education):
+        self.name = name
+        self.birth_date = birth_date
+        self.__occupation = occupation
+        self.__higher_education = higher_education
+
+    @property
+    def occupation(self):
+        return self.__occupation
+
+    @property
+    def higher_education(self):
+        return self.__higher_education
+
+    def introduce(self):
+        education_text = (
+            "У меня есть высшее образование."
+            if self.higher_education
+            else "У меня нет высшего образования."
+        )
+        print(
+            f"Привет, меня зовут {self.name}. "
+            f"Моя профессия {self.occupation}. "
+            f"{education_text}"
+        )
+
+
+class Classmate(Person):
+    def __init__(self, name, birth_date, occupation, higher_education, group_name):
+        super().__init__(name, birth_date, occupation, higher_education)
+        self.group_name = group_name
+
+    def introduce(self):
+        education_text = (
+            "У меня есть высшее образование."
+            if self.higher_education
+            else "У меня нет высшего образования."
+        )
+        print(
+            f"Привет, меня зовут {self.name}. "
+            f"Моя профессия {self.occupation}. "
+            f"Я учился с Айсулуу в группе {self.group_name}. "
+            f"{education_text}"
+        )
+
+
+class Friend(Person):
+    def __init__(self, name, birth_date, occupation, higher_education, hobby):
+        super().__init__(name, birth_date, occupation, higher_education)
+        self.hobby = hobby
+
+    def introduce(self):
+        education_text = (
+            "У меня есть высшее образование."
+            if self.higher_education
+            else "У меня нет высшего образования."
+        )
+        print(
+            f"Привет, меня зовут {self.name}. "
+            f"Моя профессия {self.occupation}. "
+            f"Мое хобби {self.hobby}. "
+            f"{education_text}"
+        )
+
+
+# Проверка
+cl1 = Classmate("Иван", "20.02.2000", "студент", True, "11D")
+cl1.introduce()
+
+fr1 = Friend("Айбек", "20.02.2000", "студент", True, "футбол")
+fr1.introduce()
